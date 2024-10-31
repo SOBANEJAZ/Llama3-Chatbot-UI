7aa1041c7ad7164073232ba6021d6a4533f33ed7
SOBANEJAZ <sobanpythonista@gmail.com>
2024-10-31 23:50:25 +0500

    added a simple chat function from Groq documention

--- cli_version.py
+++ cli_version.py
@@ -1,1 +1,24 @@
-import groq
+from groq import Groq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY"),
+)
+
+stream = client.chat.completions.create(
+    messages=[
+        {"role": "system", "content": "You are a famous podcaster, Andrew Huberman."},
+        {
+            "role": "user",
+            "content": "Who are you?",
+        },
+    ],
+    model="llama-3.2-90b-vision-preview",
+    stream=True
+)
+
+for chunk in stream:
+    print(chunk.choices[0].delta.content, end="")
